cb30eec0f18bd2632bab35b4b27ec6ad9c877f5d
kelvin-xu <58991662+kelvin-xu@users.noreply.github.com>
2019-12-20 12:35:26 -0800

    Removed unnecessary controls (#4)
    * update gitignore to ignore all the dist files
    
    * update package.json && extend webpack config to load and compile sass
    
    * reorgnize things to properly enqueue frontend and editor script/styles to the site
    
    * bug fix for anchor link when button type is full box
    
    * remove unused import
    
    * added prod build script command
    
    * move & rename files to make it more clear
    
    * fix lodash underscore conflict
    
    * add background image control
    
    * enable background image reorder
    
    * add linting for wordpress rules. Enable integration with VS code
    
    * linting fix
    
    * make preview image clickable to upload new image
    
    * massive refactor because of how the plugin handles styles originally
    
    * update ignore to include dis files in repo
    
    * remove unused style
    
    * Update block title, description. Default heading text and one bug fix
    
    * removed some controls for the block && adjust default attributes

--- src/blocks/call-to-action/inline-styles.js
+++ src/blocks/call-to-action/inline-styles.js
@@ -3,20 +3,10 @@ export default function InlineStyles( props ) {
 		blockId,
 		titleColor,
 		descColor,
-		titleFontSize,
-		descFontSize,
-		ctaFontSize,
-		ctaBtnVertPadding,
-		ctaBtnHrPadding,
-		ctaBorderStyle,
-		ctaBorderWidth,
-		ctaBorderRadius,
 		ctaBtnLinkColor,
 		ctaLinkHoverColor,
 		ctaBgColor,
 		ctaBgHoverColor,
-		ctaBorderColor,
-		ctaBorderhoverColor,
 		blockVerticalPadding,
 		blockHorizontalPadding,
 	} = props.attributes;
@@ -28,26 +18,17 @@ export default function InlineStyles( props ) {
 				}
 				#ubc-cta-block-${ blockId } .uagb-cta__title{
 					color: ${ titleColor };
-					font-size: ${ titleFontSize }px;
 				}
 				#ubc-cta-block-${ blockId } .uagb-cta__desc{
 					color: ${ descColor };
-					font-size: ${ descFontSize }px;
 				}
 				#ubc-cta-block-${ blockId } .uagb-cta__block-link{
-					font-size: ${ ctaFontSize }px;
-					padding: ${ ctaBtnVertPadding }px ${ ctaBtnHrPadding }px;
-					border-style: ${ ctaBorderStyle };
-					border-width: ${ ctaBorderWidth }px;
-					border-radius: ${ ctaBorderRadius }px;
-					border-color: ${ ctaBorderColor };
 					color: ${ ctaBtnLinkColor };
 					background-color: ${ ctaBgColor };
 				}
 				#ubc-cta-block-${ blockId } .uagb-cta__block-link:hover{
 					color: ${ ctaLinkHoverColor };
 					background-color: ${ ctaBgHoverColor };
-					border-color: ${ ctaBorderhoverColor };
 				}
 			` }
 		</style>
